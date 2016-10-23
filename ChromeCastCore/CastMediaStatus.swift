8ffef8a69137662afe6ac17ee03b233c522751a5
Guilherme Rambo <insidegui@gmail.com>
2016-10-23 17:46:47 -0200

    Added objc-compatible state property to CastMediaStatus

--- ChromeCastCore/CastMediaStatus.swift
+++ ChromeCastCore/CastMediaStatus.swift
@@ -22,6 +22,10 @@ public final class CastMediaStatus: NSObject {
     public var playerState: CastMediaPlayerState = .buffering
     public var currentTime: Double = 0
     
+    public var state: String {
+        return playerState.rawValue
+    }
+    
     public override var description: String {
         return "MediaStatus(mediaSessionId: \(mediaSessionId), playbackRate: \(playbackRate), playerState: \(playerState.rawValue), currentTime: \(currentTime))"
     }
