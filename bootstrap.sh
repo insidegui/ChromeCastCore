f5ebdacbbe5a36b258ae1f082b468b5babca4aab
Miles Hollingsworth <mhmiles@me.com>
2018-01-10 15:22:01 -0800

    remove bootstrap

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -e "\n\033[1;31mIMPORTANT: To build this project you need automake, libtool and protobuf.\nYou can install them easily with Homebrew:\n"
-echo -e "\033[1;34mbrew install automake libtool protobuf\n\n\033[0m"
-
-cd Vendor/Protobuf
-./scripts/build.sh
-cd ../..
