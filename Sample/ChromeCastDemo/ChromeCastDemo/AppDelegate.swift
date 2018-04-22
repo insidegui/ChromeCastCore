2cacb7731c20562ee464c9440ba437acdd347c80
Guilherme Rambo <insidegui@gmail.com>
2018-04-22 11:21:40 -0300

    Added skeleton for sample project

---
+++ Sample/ChromeCastDemo/ChromeCastDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChromeCastDemo
+//
+//  Created by Guilherme Rambo on 22/04/18.
+//  Copyright © 2018 Guilherme Rambo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sample/ChromeCastDemo/ChromeCastDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChromeCastDemo
+//
+//  Created by Guilherme Rambo on 22/04/18.
+//  Copyright © 2018 Guilherme Rambo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
